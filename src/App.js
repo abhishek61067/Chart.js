8fb492ceb5354a9ad0ad7506721db154a9ce373b
abhishek <abhishek.bhattarai61067@gmail.com>
2022-08-30 17:40:20 +0545

    completed upto pie chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
 
-function App() {
+import './App.css';
+import { useState } from 'react';
+import { UserData } from "./Data";
+
+
+const App = () => {
+  // chartjs is going to require two object with properties: labels, datasets
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [{
+      label: "user gained",
+      data: UserData.map((data) => data.userGain),
+      backgroundColor: ["#118888", "lightblue", "#ddccaa", "grey", "#999944"],
+      borderColor: "black",
+      borderWidth: 2,
+    }]
+  });
+  //we are now returning a jsx.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div style={{ height: 30, }}></div>
+      <div>Bar Chart</div>
+      <div style={{ width: 700 }}>
+        <BarChart chartData={userData} />
+      </div>
+      <div style={{ height: 30, }}></div>
+      <div>Line Chart</div>
+      <div style={{ width: 700 }}>
+        <LineChart chartData={userData} />
+      </div>
+      <div style={{ height: 30, }}></div>
+      <div>Pie Chart</div>
+      <div style={{ width: 700 }}>
+        <PieChart chartData={userData} />
+      </div>
     </div>
-  );
+
+  )
 }
 
 export default App;
+
+
